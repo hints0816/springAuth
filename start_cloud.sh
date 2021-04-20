72751d871f552d1a016aa80f3db49e4e984e7fb9
180686 <180686@gree.com>
2021-04-20 11:37:47 +0800

    fix(md)优化

--- start_cloud.sh
+++ start_cloud.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nohup java -jar authserver-0.0.1-SNAPSHOT.jar &
+#nohup java -jar authserver-0.0.1-SNAPSHOT.jar &
 nohup java -jar client-0.0.1-SNAPSHOT.jar &
-nohup java -jar resource-0.0.1-SNAPSHOT.jar &
+#nohup java -jar resource-0.0.1-SNAPSHOT.jar &
